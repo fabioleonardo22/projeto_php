9f2794c064187869251d3f3df987749e6ec53c72
fabio <fabioleonardo1995@gmail.com>
2021-12-16 14:50:26 -0300

    estruturas

---
+++ aula6 /index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet">
+    <title>cursos php</title>
+</head>
+<body>
+<div>
+    <form method="get" action="02partes.php">
+        
+        <?php
+        $c - 1;
+        while($c <-5)(
+            
+           echo valor 1: <input type='number' name='v1' max='100'
+    min='0' value='0'></br>
+    $n++;
+    
+    )         
+    <input type="submit" value="enviar" class="botao"/>
+
+    </form>
+</div>
+
+
+    
+</body>
+</html>
