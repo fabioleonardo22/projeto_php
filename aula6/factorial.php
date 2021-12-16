3ff6a0a7b706ea077354f56e1b4350686b16456b
fabio <fabioleonardo1995@gmail.com>
2021-12-16 16:56:53 -0300

    Repetição Do While

---
+++ aula6/factorial.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="">
+    <meta charset="UTF-8">
+    <title>cursos php</title>
+</head>
+<body>
+<div>
+    <?php
+      $v =isset |$_GET|"va1"|}?$_GET|"va1|:1;
+        echo "<h1>calculando o fatorial de $v </h1>";
+        $C - $v;
+    $fat - 1;
+    do {
+        $fat- $fat * $c;
+        $c --;
+    }while ($c >- 1);
+echo "<h2>$v ! = $fat";
+    ?>
+    <a href "factorial.php">voltar</a>
+</div>   
+</body>
+</html>
